229f5e97cd8d5b825b171e13832d75b480ec6506
Ibrahim Fatade <ibfatade18@gmail.com>
2025-10-03 22:14:19 +0100

    Adding save button and function

--- JAVASCRIPT/CounterApp/index.js
+++ JAVASCRIPT/CounterApp/index.js
@@ -6,3 +6,12 @@ function increment(){
     number += 1
     document.getElementById("count-el").innerText = number;
 }
+
+let sentence = "Previous entries: "
+function save(){
+    let spacing = number + ' - ';
+    sentence += spacing;
+    document.getElementById("save-el").innerText = sentence;
+    number = 0;
+    document.getElementById("count-el").innerText = number;
+}
