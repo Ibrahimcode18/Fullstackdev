d6e1987d9ca03a6f669614aaabab83c75b5869ff
Ibrahim Fatade <ibfatade18@gmail.com>
2025-10-04 14:12:43 +0100

    Calculator

---
+++ JAVASCRIPT/Calculator/index.js
@@ -0,0 +1,19 @@
+let number1 = 8
+let number2 = 2
+let answer = document.getElementById("sum-el")
+
+function add(){
+    answer.innerText = number1 + number2
+}
+
+function subtract(){
+    answer.innerText = number1 - number2
+}
+
+function multiply(){
+    answer.innerText = number1 * number2
+}
+
+function divide(){
+    answer.innerText = number1 / number2
+}
