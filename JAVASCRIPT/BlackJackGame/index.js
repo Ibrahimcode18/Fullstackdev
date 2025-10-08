92c4580457986e01e2068f5240fa9be9dc2e71b9
Ibrahim Fatade <ibfatade18@gmail.com>
2025-10-08 22:43:51 +0100

    addition of New Game button

--- JAVASCRIPT/BlackJackGame/index.js
+++ JAVASCRIPT/BlackJackGame/index.js
@@ -1,15 +1,25 @@
 let firstCard = 3;
 let secondCard = 5;
+let cards = [firstCard,secondCard]
 
 let sum = firstCard + secondCard;
 
 let hasBlackJack = false
 let isAlive = true
 let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
 
 function startGame(){
+    renderGame()
+}
+
+function renderGame(){
+    cardsEl.textContent = "Cards: " + cards[0] + " " +cards[1]
+    sumEl.textContent = "Sum: " + sum 
     if (sum <= 20){
-    message = "Dou you want to draw a new card?"
+        message = "Do you want to draw a new card?"
     }else if (sum === 21){
         message = "Wohoo! You've got BlackJack"
         hasBlackJack = true
@@ -17,4 +27,12 @@ function startGame(){
         message = "You're out of the game!"
         isAlive = false
     }
+    messageEl.textContent = message
+}
+
+function newCard(){
+    let number3 = 7
+    sum += number3
+    cards.push(number3)
+    renderGame()
 }
