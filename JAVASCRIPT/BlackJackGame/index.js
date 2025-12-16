e8de066f0d56b7504a512407f7a12ab930f7e90a
Ibrahim Fatade <ibfatade18@gmail.com>
2025-12-16 20:38:19 +0000

    Game modification

--- JAVASCRIPT/BlackJackGame/index.js
+++ JAVASCRIPT/BlackJackGame/index.js
@@ -1,22 +1,37 @@
-let firstCard = 3;
-let secondCard = 5;
-let cards = [firstCard,secondCard]
-
-let sum = firstCard + secondCard;
-
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
+function getRandomCard(){
+    let randomNumber = Math.floor(Math.random() * 13) + 1;
+    if (randomNumber == 1){
+        randomNumber = 11;
+    }
+    else if (randomNumber > 10){
+        randomNumber = 10;
+    }
+    return randomNumber
+}
+
 function startGame(){
+    let firstCard = getRandomCard();
+    let secondCard = getRandomCard();
+    cards = [firstCard,secondCard]
+    sum = firstCard + secondCard;
+    isAlive = true;
     renderGame()
 }
 
 function renderGame(){
-    cardsEl.textContent = "Cards: " + cards[0] + " " +cards[1]
+    cardsEl.textContent = "Cards:"
+    for (let i = 0; i < cards.length; i++){
+        cardsEl.textContent += " " + cards[i] 
+    }
     sumEl.textContent = "Sum: " + sum 
     if (sum <= 20){
         message = "Do you want to draw a new card?"
@@ -31,7 +46,10 @@ function renderGame(){
 }
 
 function newCard(){
-    let number3 = 7
+    if (isAlive === false || hasBlackJack === true){
+        return
+    }
+    let number3 = getRandomCard();
     sum += number3
     cards.push(number3)
     renderGame()
